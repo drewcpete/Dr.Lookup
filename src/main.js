45001c8067bafc9ce25dc4e81950578821f86aa7
andrew <drewcpete@gmail.com>
2019-09-13 09:51:03 -0700

    add html body and import bootstrap/jquery.  add hidden api key

--- src/main.js
+++ src/main.js
@@ -2,19 +2,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {createHTML} from "./dino.js"
-import {array} from "./dino.js"
+// import {createHTML} from "./dino.js"
+// import {array} from "./dino.js"
 
-
+$(document).ready(function(){
+  $("#lookupForm").submit(function(event) {
+    event.preventDefaul();
+    let query = $("#query").val("");
     $('#query').val("");
-    console.log(teamNum + " submit");
+    console.log(query + " is the issue");
+
+
+
     let request = new XMLHttpRequest();
-    const url = `http://dinoipsum.herokuapp.com/api?format=json&paragraphs=${teamNum}&words=${memberNum}`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170%2C%20-122.6733%2C%2025%26&skip=0&limit=10&user_key=$${process.env.exports.apiKey}&query=${query}`;
+
+
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
-        console.log(response[0]);
-        console.log(response.sort());
+        console.log(response);
+        console.log(response[1]);
+
+
       }
+
+    }
+  })
+})
