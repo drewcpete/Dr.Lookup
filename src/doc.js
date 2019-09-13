319b11a6edd5c8c6c9b187a0ae8799309dc71f25
andrew <drewcpete@gmail.com>
2019-09-13 11:24:27 -0700

    create doctor object

--- src/doc.js
+++ src/doc.js
@@ -1,3 +1,29 @@
+export class Docs {
+  constructor(issue, name){
+    this.issue = issues;
+    this.name = name;
+  }
+  getDocIssue(issue){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&user_key=${process.env.apiKey}&query=${query}`;
+      alert(url);
+
+      request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      });
+    })
+  }
+}
+
+
 
 export let popDoc = function(array){
   for (var i = 0; i < array.length; i++) {
--- src/main.js
+++ src/main.js
@@ -3,18 +3,20 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import {popDoc} from "./doc.js"
-// import {array} from "./dino.js"
+import {Doc} from "./doc.js"
 
 $(document).ready(function(){
   $("#lookupForm").submit(function(event) {
     event.preventDefaul();
     let query = $("#query").val("");
-    $('#query').val("");
+    const doctor = $("#doctorIn")
+
+    let doctors = new Doctors
     console.log(query + " is the issue");
 
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170%2C%20-122.6733%2C%2025%26&skip=0&limit=10&user_key=$${process.env.exports.apiKey}&query=${query}`;
-    console.log(url);
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&user_key=${process.env.apiKey}&query=${query}`;
+    alert(url);
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -22,7 +24,7 @@ $(document).ready(function(){
         getElements(response);
         console.log(response);
         console.log(response[0]);
-        $("#doctorOut").append(response[0])
+        $("#doctorOut").append(response.data.practices.visit_address.street + response.data.practices.visit_address.street2)
 
       }
 
