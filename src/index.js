276fe9aa790282b163fd920d00c1d9a0bdc0c6b6
andrew <drewcpete@gmail.com>
2019-09-13 12:20:47 -0700

    add creating new docs based on what input you provided

--- src/index.js
+++ src/index.js
@@ -6,26 +6,35 @@ import './styles.css';
 import {Docs} from "./doc.js"
 
 $(document).ready(function(){
-  $("#queryBtn").click(function(event) {
+  $("#queryBtn").click(function() {
     // event.preventDefaul();
-    const query = $("#query").val("");
-    const doctor = new Doctors(issue)
-    console.log(query + " is the issue");
+    const query = $("#queryIn").val();
+    $("#queryIn").val("");
+    const doctor = new Docs(query);
+    console.log(doctor.query + query + " is the searched query");
 
-    let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&user_key=${process.env.apiKey}&query=${query}`;
-    alert(url);
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-        console.log(response);
-        console.log(response[0]);
-        $("#doctorOut").append(response.data.practices.visit_address.street + response.data.practices.visit_address.street2)
-
-      }
-
-    }
+  })
+  $("#nameBtn").click(function(){
+    const docName = $("#nameIn").val()
+    $("#nameIn").val("")
+    const doctor = new Docs(name)
+    console.log(docName + " is the searched name")
   })
 })
+
+
+    // let request = new XMLHttpRequest();
+    // const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&user_key=${process.env.apiKey}&query=${query}`;
+    // alert(url);
+    //
+    // request.onreadystatechange = function() {
+    //   if (this.readyState === 4 && this.status === 200) {
+    //     const response = JSON.parse(this.responseText);
+    //     getElements(response);
+    //     console.log(response);
+    //     console.log(response[0]);
+    //     $("#doctorOut").append(response.data.practices.visit_address.street + response.data.practices.visit_address.street2)
+    //
+    //   }
+    //
+    // }
