9e5d644aa0ffde20684aa83b040c57d8de867a92
andrew <drewcpete@gmail.com>
2019-09-16 17:30:49 -0700

    fix practices array in displaying addresses and attempts to add a search by name feature

--- src/doc.js
+++ src/doc.js
@@ -1,48 +1,22 @@
 export class Docs {
-  constructor(symptom, docName){
+  constructor(symptom, name){
     this.symptom = symptom;
-    this.name = docName;
+    this.name = name;
   }
 
   getDocIssue(query){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&user_key=${process.env.apiKey}&query=${query}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&query=${query}&name=${name}&user_key=${process.env.apiKey}`;
       request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
         }
+      }
         request.open("GET", url, true);
         request.send();
-      });
-    }
-  getDocName(docName){
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&user_key=${process.env.apiKey}&name=${docName}`;
-      request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
-        }
-        request.open("GET", url, true);
-        request.send();
-      });
-    }
-
+    });
   }
-
-
-
-
-
-// export let popDoc = function(array){
-//   for (var i = 0; i < array.length; i++) {
-//     return `<p id="doc${i}out">${array[i].join(", ")}</p>`
-//   }
-// }
+}
--- src/index.js
+++ src/index.js
@@ -11,49 +11,37 @@ $(document).ready(function(){
     const query = $("#queryIn").val();
     $("#queryIn").val("");
     const doctor = new Docs(query);
-    console.log(doctor.symptom + " " + query + " is the searched query");
-
     let promise = doctor.getDocIssue(doctor.symptom);
 
     promise.then(function(response){
       const body = JSON.parse(response);
-
       let drData = body.data;
       console.log(drData[0].profile.first_name);
 
       $("#doctorOut").append(`<ul id='docList'></ul>`)
       for (var i = 0; i < drData.length; i++) {
 
-        $("#docList").append(`<li id="doc${i}"><strong>Name: </strong>${drData[i].profile.first_name + " " + drData[i].profile.last_name}<br><strong>Address: </strong> ${drData[i].practices.visit_address.street}  ${drData[i].practices.visit_address.street2}</li>`);
+        $("#docList").append(`<li id="doc${i}"><strong>Name: </strong>${drData[i].profile.first_name + " " + drData[i].profile.last_name}<br><strong>Address: </strong> ${drData[i].practices[0].visit_address.street}  ${drData[i].practices[0].visit_address.street2}</li>`);
       }
-
+    });
   });
 
-
-
-
-  })
   $("#nameBtn").click(function(){
-    const docName = $("#nameIn").val()
-    $("#nameIn").val("")
-    const doctor = new Docs(docName)
-    console.log(docName + " is the searched name")
-  })
-})
-
-
-    // let request = new XMLHttpRequest();
-    // const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5170, -122.6733, 25&skip=0&limit=10&user_key=${process.env.apiKey}&query=${query}`;
-    // alert(url);
-    //
-    // request.onreadystatechange = function() {
-    //   if (this.readyState === 4 && this.status === 200) {
-    //     const response = JSON.parse(this.responseText);
-    //     getElements(response);
-    //     console.log(response);
-    //     console.log(response[0]);
-    //     $("#doctorOut").append(response.data.practices.visit_address.street + response.data.practices.visit_address.street2)
-    //
-    //   }
-    //
-    // }
+    const name = $("#nameIn").val();
+    $("#nameIn").val("");
+    $("#doctorOut").append(`<ul id='docList'></ul>`);
+    const doctor = new Docs(name);
+    let promise = doctor.getDocIssue(doctor.name);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      let drData = body.data;
+      for (var i = 0; i < drData.length; i++) {
+        if (drData[i].profile.first_name + " " + drData[i].profile.last_name === name) {
+          console.log(name);
+          console.log(drData[i].profile.first_name + " " + drData[i].profile.last_name);
+          $("#docList").append(`<li id="doc${i}"><strong>Name: </strong>${drData[i].profile.first_name + " " + drData[i].profile.last_name}<br><strong>Address: </strong> ${drData[i].practices[0].visit_address.street}  ${drData[i].practices[0].visit_address.street2}</li>`);
+        }
+      }
+    });
+  });
+});
